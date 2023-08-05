4fa0d39dac287ba73d5eb40f0023de35ffc5157d
AlinaS13 <soulofangel13@gmail.com>
2023-08-05 12:03:06 +0300

    Adding dist

---
+++ assets/Favorites-97a9bfe0.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-7ae2c646.js";const e=()=>r.jsx("div",{children:"Favorites"});export{e as default};
---
+++ assets/Teachers-0c410722.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-7ae2c646.js";const s=()=>e.jsx("div",{children:"Teachers "});export{s as default};
