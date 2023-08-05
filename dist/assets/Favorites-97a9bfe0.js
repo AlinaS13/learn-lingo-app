acdaaacd6a674e1c20cc9d38f852264fa97c9c19
AlinaS13 <soulofangel13@gmail.com>
2023-08-05 12:55:23 +0300

    Adding dist

--- dist/assets/Favorites-97a9bfe0.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-7ae2c646.js";const e=()=>r.jsx("div",{children:"Favorites"});export{e as default};
---
+++ dist/assets/Teachers-1f6352c6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-4cfeaa3b.js";const s=()=>e.jsx("div",{children:"Teachers "});export{s as default};
