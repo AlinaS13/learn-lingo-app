98e2b806363c4b8f2848e014309be9289663808d
AlinaS13 <soulofangel13@gmail.com>
2023-08-16 08:50:40 +0300

    add dist

--- dist/assets/Favorites-4cbd7201.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-4cfeaa3b.js";const e=()=>r.jsx("div",{children:"Favorites"});export{e as default};
--- dist/assets/NotFound-6d3ef3d2.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-4cfeaa3b.js";const r=()=>o.jsx("div",{children:"Not Found "});export{r as default};
